f20f066ac60ac5c2c5688735eb24a86a33844e16
Eduardo Matuella <eduardomatuella@gmail.com>
2025-02-20 15:46:37 -0300

    att deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-chmod +x deploy.sh
+echo "Iniciando deploy..."
 
 # Navega para o diretório do frontend e executa o build
 cd frontend
+echo "Instalando dependências do frontend..."
 npm install
+echo "Executando build do frontend..."
 npm run build
+echo "Build do frontend concluído."
 
 # Volta para o diretório raiz e coleta os arquivos estáticos
 cd ..
+echo "Coletando arquivos estáticos..."
 python manage.py collectstatic --noinput
 
 # Inicia o servidor Gunicorn
+echo "Iniciando Gunicorn..."
 gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
