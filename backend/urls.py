08dcab5cc96090809e573f6d62702d8d93e7fc4d
Eduardo Matuella <eduardomatuella@gmail.com>
2025-02-20 15:38:51 -0300

    ultimos ajustes

--- backend/urls.py
+++ backend/urls.py
@@ -9,17 +9,18 @@ class ReactAppView(TemplateView):
     template_name = "index.html"
 
     def get_template_names(self):
-        # Adiciona logs para verificar o caminho
-        logging.info(f"settings.BASE_DIR: {settings.BASE_DIR}")
-        template_path = "frontend/build/index.html"
+        template_path = Path(settings.BASE_DIR) / "frontend" / "build" / "index.html"
         logging.info(f"Verificando o caminho do template: {template_path}")
         
         if not template_path.exists():
+            logging.error(f"Arquivo {template_path} não encontrado. Rode 'npm run build'.")
             raise ValueError(f"Arquivo {template_path} não encontrado. Rode 'npm run build'.")
-        return template_path
+        
+        logging.info(f"Arquivo {template_path} encontrado.")
+        return [str(template_path)]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("api.urls")),  # Mantém a API funcionando normalmente
-    re_path(r"^.*$", ReactAppView.as_view()),  # Serve o React para todas as outras rotas
+    path("api/", include("api.urls")),
+    re_path(r"^(?!api/|admin/).*", ReactAppView.as_view()),
 ]
